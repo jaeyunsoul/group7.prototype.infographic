63b14de3d1a7135377c95079572e0b9dc135ab63
jaeyunsoul <ap.glenabigail.bundalian@apecschools.edu.ph>
2026-03-19 01:02:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,36 @@ $(document).ready(function(){
   // Sliding gallery
   $("#startSlide").click(function(){
     $(".imgBox").each(function(index){
-      $(this).delay(500 * index).slideDown(800);
+      $(this).delay(400 * index).fadeIn(600);
     });
   });
 
   // Feedback panel
   $("#flip").click(function(){
-    $("#panel").slideDown("slow");
+    $("#panel").slideToggle("slow");
   });
 
-  // Change image
+  // ✅ CHANGE IMAGE (TOGGLE FIX HERE)
+  let isChanged = false;
+
   $("#changeBtn").click(function(){
-    $("#myImage").attr("src", "https://uploads.onecompiler.io/44cyrhw8k/44gqkypvd/1000016989.png");
+    if(!isChanged){
+      $("#myImage").fadeOut(200, function(){
+        $(this)
+          .attr("src", "https://uploads.onecompiler.io/44cyrhw8k/44gqkypvd/1000016989.png")
+          .fadeIn(200);
+      });
+      isChanged = true;
+      $("#changeBtn").text("Show Original");
+    } else {
+      $("#myImage").fadeOut(200, function(){
+        $(this)
+          .attr("src", "https://uploads.onecompiler.io/44cyrhw8k/44f8xzgwg/1000016211.png")
+          .fadeIn(200);
+      });
+      isChanged = false;
+      $("#changeBtn").text("Show New Image");
+    }
   });
 
 });
